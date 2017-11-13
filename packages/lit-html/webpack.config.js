f23fbff54b4f81adeb6a50d9d97b808822ba734e
Chris Esplin <chris-esplin@pluralsight.com>
2017-11-13 16:31:19 -0700

    lit-html demo

---
+++ packages/lit-html/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: "./index.js",
+  output: {
+    path: __dirname,
+    filename: "index.bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  },
+  devServer: {
+    contentBase: __dirname,
+    compress: false,
+    port: 9000
+  }
+};
