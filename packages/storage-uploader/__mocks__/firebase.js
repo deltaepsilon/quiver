d88e27d9abeb86b5ad8bff7c3204f3e0bf5cb76c
Chris Esplin <chris@chrisesplin.com>
2017-12-14 09:51:20 -0700

    Tests for storage

--- packages/storage-uploader/__mocks__/firebase.js
+++ packages/storage-uploader/__mocks__/firebase.js
@@ -1,57 +1,48 @@
 export default function Firebase() {
-  let currentUser = null;
-  class Auth {
-    set currentUser(x) {
-      currentUser = x;
+  let functions;
+  const task = {
+    on: (eventName, snapshotFn, errorFn, completeFn) =>
+      (functions = { snapshotFn, errorFn, completeFn }),
+  };
+
+  let file;
+  let path;
+
+  const ref = {
+    put: x => {
+      file = x;
+      return task;
+    },
+  };
+
+  class Storage {
+    get attributes() {
+      return { task, file, path };
     }
 
-    get currentUser() {
-      return currentUser;
+    get functions() {
+      return functions;
+    }
+
+    ref(x) {
+      path = x;
+      return ref;
     }
   }
-  let confirm = jest.fn(() => Promise.resolve());
-
-  Auth.prototype.onAuthStateChanged = jest.fn(cb => cb(currentUser));
-  Auth.prototype.signOut = jest.fn();
-  Auth.prototype.signInWithEmailAndPassword = jest.fn(() => Promise.resolve());
-  Auth.prototype.createUserWithEmailAndPassword = jest.fn(() => Promise.resolve());
-  Auth.prototype.sendPasswordResetEmail = jest.fn(() => Promise.resolve());
-  Auth.prototype.signInWithPopup = jest.fn(() => Promise.resolve());
-  Auth.prototype.signInWithRedirect = jest.fn(() => Promise.resolve());
-
-  Auth.prototype.signInWithPhoneNumber = jest.fn(() => Promise.resolve({ confirm }));
 
   class Firebase {
     constructor() {
-      this.__auth = new Auth();
+      this.__storage = new Storage();
     }
 
-    get confirm() {
-      return confirm;
-    }
-
-    set currentUser(x) {
-      this.__auth.currentUser = x;
-    }
-
-    setAuthError(method, code) {
-      this.__auth[method] = jest.fn(() => Promise.reject({ code }));
-    }
-
-    auth() {
-      return this.__auth;
+    storage() {
+      return this.__storage;
     }
 
     setConfirmError(code) {
       confirm = jest.fn(() => Promise.reject({ code }));
     }
   }
-
-  Firebase.prototype.auth.FacebookAuthProvider = jest.fn();
-  Firebase.prototype.auth.GithubAuthProvider = jest.fn();
-  Firebase.prototype.auth.GoogleAuthProvider = jest.fn();
-  Firebase.prototype.auth.TwitterAuthProvider = jest.fn();
-  Firebase.prototype.auth.RecaptchaVerifier = jest.fn();
 
   return new Firebase();
 }
--- packages/storage-uploader/src/index.spec.js
+++ packages/storage-uploader/src/index.spec.js
@@ -14,17 +14,62 @@ describe('FileUploader', () => {
     global.dispatchEvent = jest.fn();
   });
 
-  it('should render', done => {
+  let files;
+  beforeEach(() => {
+    files = [
+      new File(['one'], 'one.jpg', {
+        type: 'image/jpeg',
+      }),
+      new File(['two'], 'two.txt', {
+        type: 'application/text',
+      }),
+    ];
+  });
+
+  it('should render', () => {
     render(<Component />, container);
     htmlLooksLike(
       container.innerHTML,
       `
-      <div class="firebase-authentication" view="login-options">
-        <div name="Button" type="email" ripple="true" raised="true">{{...}}</div>
-      </div>
-    `
+        <div class="storage-uploader" view="select-files">
+          <div>
+            <input style="display: none;" type="file" id="file-input" multiple="">
+            <div name="Button" ripple="true" raised="true">{{...}}</div>
+          </div>
+        </div>
+      `
     );
-    verifyEventType('currentUserChanged').then(done, done.fail);
   });
 
+  describe('Select File', () => {
+    beforeEach(() => {
+      render(<Component />, container);
+    });
+
+    it('should respond to files', done => {
+      inputFiles(files).then(done, done.fail);
+    });
+  });
+
+  describe('Upload a file', () => {
+    
+  });
+
+  function inputFiles(files) {
+    return new Promise(resolve => {
+      const input = container.querySelector('input');
+      Object.defineProperty(input, 'files', {
+        value: files,
+      });
+
+      global.dispatchEvent = e => {
+        if (e.type == 'storageUploaderFilesChanged') {
+          expect(e.detail.files.length).toEqual(2);
+          resolve();
+        }
+      };
+
+      input.dispatchEvent(new CustomEvent('change'));
+    });
+  }
 });
